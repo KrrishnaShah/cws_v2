894a10ad49c49b136f9266bf1269a75860a32f5d
krishna kumar sah <krriss.shah@gmail.com>
2023-05-31 22:47:47 +0545

    ble testing with time sync

--- cws_v2.ino
+++ cws_v2.ino
@@ -92,6 +92,8 @@ static int __write_imu_data_to_flash_v2(void) {
   __print_buffer((uint8_t *)&imu_data, sizeof(s_imu_data_t));
 #endif
 
+  imu_data.sn = flash_header_v2.count;
+
   if (flash.writeByteArray(flash_header_v2.write_addr, (uint8_t *)&imu_data, sizeof(s_imu_data_t))) {
 #if 0
     Serial.println("Flash write success!");
@@ -103,11 +105,11 @@ static int __write_imu_data_to_flash_v2(void) {
     }
   } else {
     Serial.println("Flash write failed once!");
-    // if (flash.eraseSector(flash_header_v2.write_addr)) {
-    //   if (flash.writeByteArray(flash_header_v2.write_addr, (uint8_t *)&imu_data, sizeof(s_imu_data_t))) {
-    //     flash_header_v2.write_addr += sizeof(s_imu_data_t);
-    //   }
-    // }
+    if (flash.eraseSector(flash_header_v2.write_addr)) {
+      if (flash.writeByteArray(flash_header_v2.write_addr, (uint8_t *)&imu_data, sizeof(s_imu_data_t))) {
+        flash_header_v2.write_addr += sizeof(s_imu_data_t);
+      }
+    }
   }
 
 #if 0
@@ -144,6 +146,7 @@ static int __flash_header_v2_init(void) {
   if (FLASH_MAGIC_BYTES != flash_header_v2.magic) {
     flash_header_v2.magic = FLASH_MAGIC_BYTES;
     flash_header_v2.count = 0;
+    flash_header_v2.read_addr = 4096;
     flash_header_v2.write_addr = 4096;
   }
 
@@ -159,18 +162,16 @@ void setup() {
 
   while (!Serial) { delay(10); }
 
-  BLE.begin();
-  BLE.scanForName(ble_gateway_name);
 
   if (myIMU.begin() != 0) {
     Serial.println("Accelerometer error");
   } else {
-    Serial.println("Accelerometer OK!");
+    // Serial.println("Accelerometer OK!");
   }
 
   rtc_pcf8563.begin();
   if (rtc_pcf8563.isrunning()) {
-    Serial.println("RTC is running.");
+    // Serial.println("RTC is running.");
   } else {
     Serial.println("RTC is NOT running!");
     // rtc_pcf8563.adjust(DateTime(__DATE__, __TIME__));
@@ -217,36 +218,93 @@ void setup() {
 }
 
 
+static void send_to_ble(void) {
+  BLE.begin();
+  BLE.scanForName(ble_gateway_name);
+
+  BLEDevice peripheral = BLE.available();
+  if (peripheral) {
+    BLE.stopScan();
+
+    if (peripheral.connect()) {
+    } else {
+      Serial.println("Failed to connect!");
+    }
+
+    if (peripheral.discoverAttributes()) {
+    } else {
+      Serial.println("Attribute discovery failed!");
+      peripheral.disconnect();
+    }
+
+    BLECharacteristic imu_data_char = peripheral.characteristic(cws_data_char_uuid);
+    if (!imu_data_char) {
+      Serial.println("Peripheral does not have imu characteristic!");
+      peripheral.disconnect();
+    } else if (!imu_data_char.canWrite()) {
+      Serial.println("Peripheral does not have a writable imu characteristic!");
+      peripheral.disconnect();
+    }
+
+    BLECharacteristic imu_time_char = peripheral.characteristic(cws_time_char_uuid);
+    if (!imu_time_char) {
+      Serial.println("Peripheral does not have imu characteristic!");
+      peripheral.disconnect();
+    } else if (!imu_time_char.canRead()) {
+      Serial.println("Peripheral does not have a readable imu characteristic!");
+      peripheral.disconnect();
+    }
+
+    if (peripheral.connected()) {
+      uint8_t ble_buffer[512];
+
+      while (flash_header_v2.read_addr <= flash_header_v2.write_addr) {
+        uint32_t byte_len_diff = (flash_header_v2.write_addr - flash_header_v2.read_addr);
+        uint32_t read_len = byte_len_diff > 512 ? 512 : byte_len_diff;
+        if (flash.readByteArray(flash_header_v2.read_addr, ble_buffer, read_len)) {
+          if (imu_data_char.writeValue(ble_buffer, read_len)) {
+            flash_header_v2.read_addr += 512;
+          } else {
+            Serial.println("BLE write Failed");
+          }
+        }
+      }
+
+      time_t epoch_time = 0;
+      if (imu_time_char.readValue(&epoch_time, sizeof(time_t))) {
+        rtc_pcf8563.adjust(DateTime(epoch_time));
+      } else {
+        Serial.println("Ble time read failed!");
+      }
+
+      BLE.disconnect();
+      Serial.println(epoch_time);
+      rtc_pcf8563 now;
+
+    } else {
+      Serial.println("BLE connection failed!");
+    }
+  }
+}
+
 void loop() {
   digitalWrite(LED_BUILTIN, 1);
   s_imu_data_t *tmp_imu_data = __updateAccelerometer();
   __write_imu_data_to_flash_v2();
 
-#if 0
-  __write_data_to_flash(tmp_imu_data);
-  __update_flash_header();
-#endif
+  if (flash_header_v2.read_addr + (16 * 32 * 10) < flash_header_v2.write_addr) {
+    send_to_ble();
 
-  DateTime now = rtc_pcf8563.now();
-#if 0
-  char buf[100];
-  strncpy(buf, "DD.MM.YYYY hh:mm:ss", 100);
-  Serial.println(now.toString(buf));
-#endif
-
-  uint32_t epoch32 = now.unixtime();
-  // Serial.print("epoch time now: ");
-  Serial.println(epoch32);
-
-#if 0
-  DateTime converted_time(epoch32);
-  strncpy(buf, "converted -> DD.MM.YYYY hh:mm:ss", 100);
-  Serial.println(converted_time.toString(buf));
-#endif
+    DateTime now = rtc_pcf8563.now();
+    imu_data.epoch_time = now.unixtime();
+    Serial.print("epoch now: ");
+    Serial.println(imu_data.epoch_time);
+  }
 
 #if 0
   BLEDevice peripheral = BLE.available();
   if (peripheral) {
+#if 0
     Serial.print("Found ");
     Serial.print(peripheral.address());
     Serial.print(" '");
@@ -254,20 +312,21 @@ void loop() {
     Serial.print("' ");
     Serial.print(peripheral.advertisedServiceUuid());
     Serial.println();
+#endif
 
     BLE.stopScan();
-    Serial.println("Connecting ...");
+    // Serial.println("Connecting ...");
 
     if (peripheral.connect()) {
-      Serial.println("Connected");
+      // Serial.println("Connected");
     } else {
       Serial.println("Failed to connect!");
     }
 
     // discover peripheral attributes
-    Serial.println("Discovering attributes ...");
+    // Serial.println("Discovering attributes ...");
     if (peripheral.discoverAttributes()) {
-      Serial.println("Attributes discovered");
+      // Serial.println("Attributes discovered");
     } else {
       Serial.println("Attribute discovery failed!");
       peripheral.disconnect();
@@ -298,7 +357,7 @@ void loop() {
       imu_time_char.readValue(&epoch_time, sizeof(time_t));
       Serial.println(epoch_time);
 
-      delay(20);
+      // delay(20);
     }
 
     BLE.scanForName(ble_gateway_name);
@@ -312,8 +371,6 @@ void loop() {
 }
 
 static s_imu_data_t *__updateAccelerometer(void) {
-  memset(&imu_data, 0, sizeof(s_imu_data_t));
-
   imu_data.acc_val_x = (int)(myIMU.readFloatAccelX() * 100);
   imu_data.acc_val_y = (int)(myIMU.readFloatAccelY() * 100);
   imu_data.acc_val_z = (int)(myIMU.readFloatAccelZ() * 100);
@@ -340,6 +397,11 @@ static s_imu_data_t *__updateAccelerometer(void) {
 
   DateTime now = rtc_pcf8563.now();
   imu_data.epoch_time = now.unixtime();
+  Serial.print("epoch now: ");
+  Serial.println(imu_data.epoch_time);
+  if (now.year() < 2023) {
+    send_to_ble();
+  }
   // Serial.print("epoch time: ");
   // Serial.println(imu_data.epoch_time);
 
